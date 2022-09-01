a498263a063f8cf9a20d1c20006c63bd10235675
pawanxgill <g.pawandeep@gmail.com>
2022-09-01 14:19:35 -0700

    Initialized DSA folder with node & linked lists

---
+++ node.js
@@ -0,0 +1,51 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+    this.previous = null;
+  }
+
+  setNextNode(node) {
+    if (node instanceof Node || node === null) {
+      this.next = node;
+    } else {
+      throw new Error('Next node must be a member of the Node class')
+    }
+  }
+
+  setPreviousNode(node) {
+    if (node instanceof Node || node === null) {
+      this.previous = node;
+    } else {
+      throw new Error('Previous node must be a member of the Node class')
+    }
+  }
+
+  getNextNode() {
+    return this.next;
+  }
+
+  getPreviousNode() {
+    return this.previous;
+  }
+}
+
+
+// const strawberryNode = new Node('Berry Tasty');
+// const vanillaNode = new Node('Vanilla');
+// const coconutNode = new Node('Coconuts for Coconut');
+
+// vanillaNode.setNextNode(strawberryNode);
+// strawberryNode.setNextNode(coconutNode);
+
+// let currentNode = vanillaNode;
+
+// while (currentNode !== null) {
+//   console.log(currentNode.data)
+//   currentNode = currentNode.next
+// }
+
+
+
+
+module.exports = Node;
