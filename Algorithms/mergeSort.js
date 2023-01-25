22def996c22df165ed71999f17e127feb7ab2608
pawanxgill <g.pawandeep@gmail.com>
2023-01-25 13:01:19 -0800

    added quick sort algorithm

--- Algorithms/mergeSort.js
+++ Algorithms/mergeSort.js
@@ -26,11 +26,6 @@ const mergeSort = (startArray) => {
     return sortedArray.concat(leftArray).concat(rightArray);
   }
   
-  
-  const inputArr = [3, 5, 2, 90, 4, 7];
-  
-  console.log(mergeSort(inputArr));
-  
   module.exports = {
     mergeSort
   };
